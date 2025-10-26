f8412d37fe34398dbe14f510401f663637cc5514
Strelkov KM <strelkovkm96@outlook.com>
2025-10-26 10:45:17 +0300

    v 1.5.2 Bugfix: main

--- SQApp/test_debug.cpp
+++ SQApp/test_debug.cpp
@@ -3,23 +3,5 @@
 #include "TMultiStack.h"
 
 int main() {
-	TMultiStack<int> M(9, 3);
-	M.Put_M(0, 1);
-	M.Put_M(0, 8);
-	M.Put_M(2, 1);
-	M.Put_M(2, 5);
-	M.Put_M(2, 18);
-	M.Put_M(2, 9); //Repack
-	cout << M << " " << M.GetCapacity_M() << endl;
-	M.SayStatusStack();
-	M.PushStack_M(1, 2);
-	M.Put_M(1, 2);
-	M.Put_M(1, 1);
-	cout << M << " " << M.GetCapacity_M() << endl;
-	M.SayStatusStack();
-	M.PopStack_M(1);
-	cout << M << " " << M.GetCapacity_M() << endl;
-	M.SayStatusStack();
-
 
 }
